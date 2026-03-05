fbe6bc5e6c0f7e47101e29fb8cb213f550234b80
shadowgardent <142168984+shadowgardent@users.noreply.github.com>
2026-03-05 12:33:18 +0700

    update qr generator

--- api/r.js
+++ api/r.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   }
 
   const API =
-  "https://script.google.com/macros/s/AKfycbyfnkXX-qFYA1rdT-K1J7-F99IJMLBvBs03Z50rp1wdLsBWo4-FUmexjV5OVxWdo4wQCQ/exec"
+  "https://script.google.com/macros/s/AKfycbyfnkXX-qFYA1rdT-K1J7-F99IJMLBvBs03Z50rp1wdLsBWo4-FUmexjV5OVxWdo4wQCQ/exec?id=" + id;
 
   const response = await fetch(API);
   const data = await response.json();
